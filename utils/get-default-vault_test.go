dfd347a83a9e5fd2d77c7f5107e1961be2d20b3b
Kartikay Jainwal <kartikayjainwal@gmail.com>
2023-04-13 19:52:28 +0100

    tests: wip GetDefaultVault

--- utils/get-default-vault_test.go
+++ utils/get-default-vault_test.go
@@ -1,25 +1,55 @@
 package utils_test
 
-import "testing"
+import (
+	"encoding/json"
+	"github.com/Yakitrak/obsidian-cli/utils"
+	"log"
+	"os"
+	"testing"
+)
 
-func TestGetDefaultVault(t *testing.T) {
-	// TODO get full coverage
+func TestGetDefaultVaultWithVaultName(t *testing.T) {
+	expectedVaultName := "myVault"
 
-	// Set up default vault
-	SetDefaultVault("new default value")
+	// Call the function with a non-empty vaultName
+	result := utils.GetDefaultVault(expectedVaultName)
 
-	var tests = []struct {
-		testName string
-		want     string
-	}{
-		{"Sets default vault", "new default value"},
-	}
-	for _, tt := range tests {
-		t.Run(tt.testName, func(t *testing.T) {
-			got := GetDefaultVault("")
-			if got != tt.want {
-				t.Errorf("got %s, want %s", got, tt.want)
-			}
-		})
+	// Assert that the result matches the expected vaultName
+	if result != expectedVaultName {
+		t.Errorf("Expected vault name '%s', but got '%s'", expectedVaultName, result)
 	}
 }
+
+func TestGetDefaultVaultWithoutVaultName(t *testing.T) {
+	expectedVaultName := "defaultVault"
+
+	// Create a temporary config file with the default vault name
+	tmpfile, err := os.CreateTemp("", "config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(tmpfile.Name())
+
+	config := utils.Config{
+		DefaultVaultName: expectedVaultName,
+	}
+	configBytes, err := json.Marshal(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = tmpfile.Write(configBytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmpfile.Close()
+
+	// Call the function without a vaultName
+	result := utils.GetDefaultVault("")
+
+	// Assert that the result matches the expected default vault name
+	if result != expectedVaultName {
+		t.Errorf("Expected default vault name '%s', but got '%s'", expectedVaultName, result)
+	}
+}
+
+//func TestGetDefaultVaultWithInvalidConfigFile(t *testing.T) {
