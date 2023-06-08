4439683c8405fde343ff1cf25268a28d8a4fc6e3
Kartikay Jainwal <kartikayjainwal@gmail.com>
2023-06-08 18:46:45 +0100

    feat: release to windows with scoop

--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "obs",
 	Short:   "obs - CLI to open, search, move, create and update notes",
-	Version: "v0.1.4",
+	Version: "v0.1.5",
 	Long:    "obs - CLI to open, search, move, create and update notes",
 }
 
