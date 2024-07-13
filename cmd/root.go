848cb3a4c1559f5f846adaf1f301891b2f9fa426
Kartikay Jainwal <kartikayjainwal@gmail.com>
2024-07-13 18:06:59 +0100

    chore: correct version on command

--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "obsidian-cli",
 	Short:   "obsidian-cli - CLI to open, search, move, create and update notes",
-	Version: "v0.1.6",
+	Version: "v0.1.7",
 	Long:    "obsidian-cli - CLI to open, search, move, create and update notes",
 }
 
